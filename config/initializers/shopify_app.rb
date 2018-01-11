dc8420748ecd347112befc32d7447042a45e5459
Dylan Harris <harrisdylan1@outlook.com>
2018-01-11 03:12:53 +0000

    created shopify app and made app ready to call from shopify api

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "059da0d4e1ae8840b09ee8ed1aa5ed65"
+  config.secret = "dcec44e1de95fd8eec726fbb883bc315"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
